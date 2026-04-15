435003c55e9ecfd38afc52200fb19dd4a3b0c83e
OisinR774 <oisinr774@gmail.com>
2026-04-15 22:24:08 +0100

    some minor changes

---
+++ Parallax2D/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    public GameObject container;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Escape))
+        {
+            container.SetActive(true);
+            Time.timeScale = 0;
+        }
+        
+    }
+
+    public void ResumeButton()
+    {
+        container.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    public void MainMenuButton()
+    {
+        SceneManager.LoadScene("TitleScreen");
+    }
+}
