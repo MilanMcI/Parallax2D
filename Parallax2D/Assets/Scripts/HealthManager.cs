45c5e37350964a18bacf743abe1fe92af5cf7264
OisinR774 <oisinr774@gmail.com>
2026-04-18 15:50:36 +0100

    Death Scene

--- Parallax2D/Assets/Scripts/HealthManager.cs
+++ Parallax2D/Assets/Scripts/HealthManager.cs
@@ -1,4 +1,8 @@
+using System.Collections;
+using System.Runtime.CompilerServices;
 using UnityEngine;
+using UnityEngine.Experimental.Rendering;
+using UnityEngine.SceneManagement;
 
 // Just a simple health manager to show how you can controll the HealthBar UI
 public class HealthManager : MonoBehaviour
@@ -6,12 +10,19 @@ public class HealthManager : MonoBehaviour
     public int maxHealth = 100;
     public int currentHealth;
 
+    public GameObject DeathScreen; 
+
+
     public HealthBar healthBar;
+    private Rigidbody2D rb;
+    private SpriteRenderer SpriteRenderer;
 
     void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
         currentHealth = maxHealth;
         healthBar.SetMaxHealth(maxHealth);
+        SpriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     void Update()
@@ -28,5 +39,46 @@ public class HealthManager : MonoBehaviour
         currentHealth -= damage;
 
         healthBar.SetCurrentHealth(currentHealth);
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Damage"))
+        {
+            TakeDamage(25);
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, 25);
+            StartCoroutine(BlinkRed());
+        }
+
+    }
+
+    private IEnumerator BlinkRed()
+    {
+        SpriteRenderer.color = Color.red;
+        yield return new WaitForSeconds(0.1f);
+        SpriteRenderer.color = Color.white;
+    }
+
+    private void Die()
+    {
+        DeathScreen.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    public void RestartButton()
+        {
+            SceneManager.LoadScene("Level1");
+            Time.timeScale = 1;
+        }   
+
+    public void MainMenuButton()
+        {
+            SceneManager.LoadScene("TitleScreen");
+            Time.timeScale = 1;
+        }
 }
+
